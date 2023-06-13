8f9865cb8f44a68ee2e6b07776b0f91924b8e104
dancingKim <rightlightfg@gmail.com>
2023-06-13 15:33:48 +0900

    Change project to use gradle, spring boot

---
+++ src/main/java/com/jungsukcd2/postory/PostoryBackApplication.java
@@ -0,0 +1,13 @@
+package com.jungsukcd2.postory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostoryBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostoryBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jungsukcd2/postory/PostoryBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jungsukcd2.postory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostoryBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
